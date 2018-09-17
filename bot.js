852c669d56dc1d99fef75815a15ba6ca77bc0831
dadofme <43331798+dadofme@users.noreply.github.com>
2018-09-17 10:07:18 -0700

    Lol i made it loop

--- bot.js
+++ bot.js
@@ -35,10 +35,10 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         switch(cmd) {
             // !ping
             case 'ping':
-                bot.sendMessage({ to: channelID, message: 'Pong!' });
+                bot.sendMessage({ to: channelID, message: '!Pong' });
             break;
             case 'pong':
-                bot.sendMessage({ to: channelID, message: 'Ping!' });
+                bot.sendMessage({ to: channelID, message: '!Ping' });
             break;
             default:
                 bot.sendMessage({ to: channelID, message: 'Unknown command.' });
